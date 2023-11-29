937d4e8bfb09911f371db1ef5b437884636c88ce
ProfMariaClara <129232555+ProfMariaClara@users.noreply.github.com>
2023-11-29 14:48:49 -0300

    Enter

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-function tocaSomGrilo () {
-    document.querySelector("#som_tecla_grilo").play ();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
-const listadeTeclas = document.querySelectorAll(".tecla");
-listadeTeclas[4].onclick = tocaSomGrilo
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    } 
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa')
+        }
+        
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+    }
+}
